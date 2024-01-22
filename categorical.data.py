a8f66796a146b508b6a82dbabe2dbebe6684244c
Çağla Nur Baran <130457927+calalupsi@users.noreply.github.com>
2024-01-23 01:21:09 +0300

    Update categorical.data.py

--- categorical.data.py
+++ categorical.data.py
@@ -70,4 +70,21 @@ s2=pd.concat([s,sonuc3], axis=1)
 print(s2)
 
 
+
+from sklearn.model_selection import train_test_split
+
+x_train,x_test,y_train,y_test = train_test_split(s,sonuc3,test_size=0.33, random_state=0)
+
+from sklearn.preprocessing import StandardScaler
+
+sc=StandardScaler()
+X_train = sc.fit_transform(x_train)
+X_test = sc.fit_transform(x_test)
+
+    
+
+
+print(s2)
+
+
     
